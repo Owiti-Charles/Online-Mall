3a1a67eff3afa4340efc832a3df672e8d6100a88
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-02-26 14:22:08 +0300

    display dummy seeded products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,8 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        return view('home');
+    {   
+        $products = Product::take(20)->get();
+        return view('home', ['products' => '$products']);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,18 @@
 <div class="container">
     <h2>Products</h2>
     <div class="row">
-        
+        @foreach ($produts as product)
+            <div class="card">
+            <img class="card-img-top" src="img.jpg" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">Title</h4>
+                <p class="card-text">Text</p>
+            </div>
+            <div class="card-body">
+                <a href="#" class="card-link">Add to Cart</a>
+            </div>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
